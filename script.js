783773b3df2711e3e1f63237bd1eda1fdcc94941
Matvii Rubets <rmatvey11@gmail.com>
2023-11-12 16:02:14 +0100

    init

---
+++ script.js
@@ -0,0 +1,26 @@
+let acc = document.getElementsByClassName("accordion-item");
+
+for (let i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function () {
+		this.classList.toggle("active");
+		let panel = this.nextElementSibling;
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+		}
+	});
+}
+
+const toggleBtn = document.querySelector(".burger");
+const toggleBtnIcon = document.querySelector(".burger img");
+const dropDownMenu = document.querySelector(".dropdown-menu");
+
+toggleBtn.onclick = function () {
+	dropDownMenu.classList.toggle("open");
+	const isOpen = dropDownMenu.classList.contains("open");
+
+	toggleBtnIcon.classList = isOpen
+		? (toggleBtnIcon.src = "./assets/icons/close.png")
+		: (toggleBtnIcon.src = "./assets/icons/burger.png");
+};
