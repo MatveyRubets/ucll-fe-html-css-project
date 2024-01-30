0b1f0cf14327f14813ecd313e66f8ea36f18343a
Matvii Rubets <rmatvey11@gmail.com>
2024-01-30 14:23:14 +0100

    lang v1

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+const userLang = navigator.language || navigator.userLanguage;
+const supportedLanguages = ["pl", "ua"];
+
+function loadContent(language) {
+	const filename = "index_" + language + ".html";
+	fetch(filename)
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("File not found or error loading language file");
+			}
+			return response.text();
+		})
+		.then((html) => {
+			document.querySelector("html").innerHTML = html;
+		})
+		.catch((error) => {
+			console.error("Error loading language file:", error);
+			// Load another file or perform fallback action here
+			loadFallbackContent();
+		});
+}
+
+function loadFallbackContent() {
+	// Load another file or perform fallback action here
+	// For example, load a default language or show an error message
+	loadContent("pl");
+}
+
+if (supportedLanguages.includes(userLang)) {
+	loadContent(userLang);
+} else {
+	loadFallbackContent();
+}
 let acc = document.getElementsByClassName("accordion-item");
 
 for (let i = 0; i < acc.length; i++) {
@@ -15,12 +48,16 @@ for (let i = 0; i < acc.length; i++) {
 const toggleBtn = document.querySelector(".burger");
 const toggleBtnIcon = document.querySelector(".burger img");
 const dropDownMenu = document.querySelector(".dropdown-menu");
+console.log(toggleBtn);
+console.log(toggleBtnIcon);
+console.log(dropDownMenu);
 
-toggleBtn.onclick = function () {
+toggleBtn.addEventListener("click", () => {
+	console.log("object");
 	dropDownMenu.classList.toggle("open");
 	const isOpen = dropDownMenu.classList.contains("open");
 
 	toggleBtnIcon.classList = isOpen
 		? (toggleBtnIcon.src = "./assets/icons/close.png")
 		: (toggleBtnIcon.src = "./assets/icons/burger.png");
-};
+});
